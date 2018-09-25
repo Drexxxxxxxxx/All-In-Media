cd2906f60ff96e2625a68cd006920dcfb428a356
Andre Silva <asilva@I9.innovagency.com>
2018-09-25 14:30:55 +0100

    14.30

--- AdicionarConteudo/bd_conect.php
+++ AdicionarConteudo/bd_conect.php
@@ -1,14 +1,15 @@
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-  <style type="text/css">
-  
-  </style>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="stylesheet/style.css" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="stylesheet/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="..\_MasterPage\stylesheets\css_index.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
@@ -17,14 +18,130 @@
 
 
 <body>
- <form method="post" enctype="multipart/form-data">
-Texto da publicação<input type="text" name="titulo" id="titulo">
-<input type="file" name="image">
-<br>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <input type="image" class="logo" src="..\_MasterPage\images/Logo2.png" alt="">
+            </a>
+            <button class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon" style="color:black"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <div class="mx-auto d-block text-center" style="width:80%">
+                    <input type="search" name="" placeholder="Search whateaver you want..." id="searchInput">
+                </div>
+                <div class="sidenav_2">
+                <nav class="nav justify-content-center">
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">
+                        <a class="nav-link hrefs" href="#">Trending</a>
+                    </div>
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">
+                        <a class="nav-link hrefs" href="#">Canais Subs</a>
+                    </div >
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">     
+                        <a class="nav-link hrefs" href="#">Favoritos</a>
+                    </div>
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">
+                        <a class="nav-link hrefs" href="#">Meus Videos</a>
+                    </div>
+                                        
+                </nav>
+           
+            </div> 
+            <ul class="navbar-nav ml-auto">
+                <nav class="nav justify-content-center">
+                    <a class="nav-link icns" style="padding:10px"  href="#"><i class="fas fa-search"></i></a>
+                    <a class="nav-link icns" style="padding:10px" href="#"><i class="fas fa-cog"></i></a>
+                    <a class="nav-link icns" style="padding:10px" href="#"><i class="fas fa-user"></i></a>
+                                                        
+                </nav>
+                                                 
+            </ul> 
+                                           
+            </ul>
+            </div>  
+                                    
+            </div>
+                        
+        </nav>
+               
+                        <div id="sidenav" >
+                     
+                                <div id="sidenav_menu">
+                                   <nav class="nav justify-content-center sidenav">
+                                       <a class="nav-link" href="#">Trending</a>
+                                       <a class="nav-link" href="#">Canais Subs</a>
+                                       <a class="nav-link" href="#">Favoritos</a>
+                                       <a class="nav-link" href="#">Meus Videos</a>
+                                     </nav>
+                                 
+                                   
+                                </div> 
+                                 
+                                    
+                           </div>  
+                
+ 
+    
+        </header>
 
-<input type="submit" name="submit" value="Submit">
-</form>
+    <div class="container">
+            <form method="post" enctype="multipart/form-data">
+            <div>
+                <hr>
+                <h1> Create Post </h1>
+                <hr>
+                <div class="row">
+                    <div class="col-lg-8 col-md-8" style="width: 100%;">
+                        <div class="input_form" style="">
+                            <input class="txt_titulo" type="text" class="card-title" placeholder="Title" name="titulo" id="titulo"> <br><br>
+                            <textarea class="txt_textOpcional"  placeholder="Text(Optional)" name="description" id="description"></textarea>                    
+                      
+                        </div>
+                          
+                    </div>
+                    <div class="col-lg-4 col-md-4 "> 
+                        <div class="input_form" >
+                            <img src="images/video_plc.jpg" width="100%" height="100%" alt="" srcset="">
+                        </div>
+                    </div> 
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="input_form" >
+                            <label for="file-input">
+                                <i class="fas fa-file-upload"></i>
+                            </label>
+                            <input class="file-upload" id="file-input" type="file" name="image"><br>
+                            <input type="submit" name="submit" value="Post">
+                        </div>
+                       
+                    </div> 
+                </div>
+                
+            </div>
+            <hr>  
+            <br>        
+            
+        </form>
+    </div>    
+
+
+
+ </body>
+ 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
+
+
+
+
+
 <?php
+
 if(isset($_POST['submit'])){
   if(getimagesize($_FILES['image']['tmp_name'])==FALSE){
       echo"failed";
@@ -47,33 +164,9 @@ if($query){
 }
 mysqli_close($con);
 }
-display();
-function display(){
-  $con = mysqli_connect("localhost","root","", "phpteste");
-  $sql = "select * from conteudo";
-   $query=mysqli_query($con,$sql);
-  $num=mysqli_num_rows($query);
-  for($i=0;$i<$num;$i++){
-      $result=mysqli_fetch_array($query);
-      $img=$result['imagem'];
-      echo '<img class="img" src="data:image;base64,'.$img.'"><br>';
-  }
-}
+
 
 
 
 ?>
-
- </body>
- 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</html>
-
-
-
-
-
-
 
