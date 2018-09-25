c99e32571a11f67c51fd37c5f36f0ad17d84deab
Andre Silva <asilva@I9.innovagency.com>
2018-09-25 15:44:49 +0100

    15.44

--- AdicionarConteudo/video.php
+++ AdicionarConteudo/video.php
@@ -1,52 +1,137 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style type="text/css">
-  .img{
- 
-      object-position:center;
-      width:200px;
-      height:200px
-  }
-  </style>
-  </head>
+  <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="stylesheet/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="..\_MasterPage\stylesheets\css_index.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+</head>
 <body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <input type="image" class="logo" src="..\_MasterPage\images/Logo2.png" alt="">
+            </a>
+            <button class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon" style="color:black"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <div class="mx-auto d-block text-center" style="width:80%">
+                    <input type="search" name="" placeholder="Search whateaver you want..." id="searchInput">
+                </div>
+                <div class="sidenav_2">
+                <nav class="nav justify-content-center">
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">
+                        <a class="nav-link hrefs" href="#">Trending</a>
+                    </div>
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">
+                        <a class="nav-link hrefs" href="#">Canais Subs</a>
+                    </div >
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">     
+                        <a class="nav-link hrefs" href="#">Favoritos</a>
+                    </div>
+                    <div class="col-xs-6 col-sm-6" style="padding:15px">
+                        <a class="nav-link hrefs" href="#">Meus Videos</a>
+                    </div>
+                                        
+                </nav>
+           
+            </div> 
+            <ul class="navbar-nav ml-auto">
+                <nav class="nav justify-content-center">
+                    <a class="nav-link icns" style="padding:10px"  href="#"><i class="fas fa-search"></i></a>
+                    <a class="nav-link icns" style="padding:10px" href="#"><i class="fas fa-cog"></i></a>
+                    <a class="nav-link icns" style="padding:10px" href="#"><i class="fas fa-user"></i></a>
+                                                        
+                </nav>
+                                                 
+            </ul> 
+                                           
+            </ul>
+            </div>  
+                                    
+            </div>
+                        
+        </nav>
+               
+                        <div id="sidenav" >
+                     
+                                <div id="sidenav_menu">
+                                   <nav class="nav justify-content-center sidenav">
+                                       <a class="nav-link" href="#">Trending</a>
+                                       <a class="nav-link" href="#">Canais Subs</a>
+                                       <a class="nav-link" href="#">Favoritos</a>
+                                       <a class="nav-link" href="#">Meus Videos</a>
+                                     </nav>
+                                 
+                                   
+                                </div> 
+                                 
+                                    
+                           </div>  
+                
+ 
+    
+        </header>
+        <div class="container">
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <div>
+                <hr>
+                <h1> Create Post </h1>
+                <hr>
+                <div class="row">
+                    <div class="col-lg-8 col-md-8" style="width: 100%;">
+                        <div class="input_form" style="">
+                            <input class="txt_titulo" type="text" class="card-title" placeholder="Title" name="titulo" id="titulo"> <br><br>
+                            <textarea class="txt_textOpcional"  placeholder="Text(Optional)" name="description" id="description"></textarea>                    
+                      
+                        </div>
+                          
+                    </div>
+                    <div class="col-lg-4 col-md-4 preview "> 
+                        <div class="input_form" >
+                            <img src="images/image_plc.jpg" width="100%" height="100%" alt="" srcset="">
+                        </div>
+                    </div> 
+                </div>
+                <div class="row" style="margin-top:10px">
+                    <div class="col-lg-12 col-md-12">
+                        <div style="text-align:center" >
+                            <label  for="file-input">
+                                <div id="load_icon"></div>
+                            </label> 
+                            <label  for="submit_post">
+                                <div id="submit_icon"></div>
+                            </label>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-Texto da publicação<input type="text" name="titulo" id="titulo">
-   Select image to upload:
-   <input type="file" name="fileToUpload" id="fileToUpload" accept="video/*">
 
-   <input type="submit" value="Upload Image" name="submit">
+                            <input class="file-upload" id="file-input" name="fileToUpload" type="file" accept="video/*">
+                            <input type="submit" id="submit_post" name="submit" value="Post">
+                        </div>
+                       
+                    </div> 
+                </div>
+                
+            </div>
+            <hr>  
+            <br>        
+            
+        </form>
+    </div>    
   
 </form>
 
 
  </body>
-<?php
-display();
-function display(){
-  $con = mysqli_connect("localhost","root","", "phpteste");
-  $sql = "select * from conteudo";
-   $query=mysqli_query($con,$sql);
-  $num=mysqli_num_rows($query);
-  for($i=0;$i<$num;$i++){
-      $result=mysqli_fetch_array($query);
-      $video=$result['video'];
-      if($video=="")
-      {
-      }
-      else{
-      echo '<video width="400" height="300" controls>';
-      echo '<source class="img" src="'.$video.'" type="video/mp4">';
-      echo '<source class="img" src="'.$video.'" type="video/ogg"><br><br><br>';
-     echo '</video>';
-      }
-     
-  }
-}
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-?>
 
 </html>
-
