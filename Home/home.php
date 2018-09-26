aba7e236e6260c589f3537d713b9cbda32c96b8d
Miguel Boavida <mboavida@I9.innovagency.com>
2018-09-26 16:57:59 +0100

    sdfdasfdsfdsf

--- Home/home.php
+++ Home/home.php
@@ -105,27 +105,11 @@ function display(){
       if(!$img=="")
       { 
       echo '<img class="img" src="data:image;base64,'.$img.'"><br><label class="container">Like
-      <input type="radio" value="radio" name="radio'.$id.'" onchange="myFunction()"><script>
-  
-  function myFunction() {
-  alert("radio selected'.$id.'");
-  
-  }
-  
-   </script>
+      <input type="radio" value="radio" name="radio'.$id.'" onchange="myFunction('.$id.')">
       <span class="checkmark"></span>
     
     Dislike
-      <input type="radio" value="radio" name="radio'.$id.'" onchange="myFunction()"><script>
-  
-      function myFunction() {
-        alert("radio selected'.$id.'");;
-        <?php 
-        $i = 0;
-
-      }
-      
-       </script>
+      <input type="radio" value="radio" name="radio'.$id.'" onchange="myFunction('.$id.')">
       <span class="checkmark"></span>
     </label><textarea rows="3" cols="50">
    
@@ -178,4 +162,12 @@ function display(){
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="script.js"></script>
+<script>
+  
+  function myFunction(ola) {
+  alert("radio selected"+ola);
+  
+  }
+  
+   </script>
 </html>
