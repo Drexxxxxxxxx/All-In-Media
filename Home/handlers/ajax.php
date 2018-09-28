4f28cd00c819dd5c72761d1ce24872b6425e9cf4
Miguel Boavida <mboavida@I9.innovagency.com>
2018-09-28 09:45:20 +0100

    ola

--- Home/handlers/ajax.php
+++ Home/handlers/ajax.php
@@ -1,31 +1,20 @@
 <?php
-echo "<script>alert('test');</script>";
 include("../config.php");
 if( isset($_REQUEST['action']) ){
 	switch( $_REQUEST['action'] ){
-		case "Likes_Dislikes":
+		case "AdicionarPessoa":
 		$con = mysqli_connect("localhost","root","", "phpteste");
 		$sql="update conteudo SET likes = likes + 1 WHERE id = " .$_REQUEST['ola']. "";
 		$query=mysqli_query($con,$sql);
 		if($query){
-			echo"sucess";
-			
+			echo"sucess";	
 		}
 		mysqli_close($con);
-		break;		
-	}
-}
-?>
-
-<?php
-include("../config.php");
-if( isset($_REQUEST['action']) ){
-	switch( $_REQUEST['action'] ){
-		case "SendMessage":
 		break;
-		case "AdicionarPessoa":
+
+		case "Dislike":
 		$con = mysqli_connect("localhost","root","", "phpteste");
-		$sql="update conteudo SET likes = likes + 1 WHERE id = " .$_REQUEST['ola']. "";
+		$sql="update conteudo SET dislikes = dislikes + 1 WHERE id = " .$_REQUEST['ola']. "";
 		$query=mysqli_query($con,$sql);
 		if($query){
 			echo"sucess";	
